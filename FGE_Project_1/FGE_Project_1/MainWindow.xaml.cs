b710dcef25bf2daae97fa01fffdc4fda286d5dec
lilolol <33486364+lilolol@users.noreply.github.com>
2017-11-08 23:29:57 +0300

    Add files via upload
    Добавлен слайдер изменения размера кисти

---
+++ FGE_Project_1/FGE_Project_1/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Reflection;
+
+namespace FGE_Project_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MenuItem menuItem = (MenuItem)sender;
+            MessageBox.Show(menuItem.Header.ToString());
+        }
+
+        //обработчик SizeSlider_ValueChanged, который будет срабатывать при возникновении события ValueChanged - изменении значения слайдера и изменять толщину кисти в соответсвтии с ним
+       private void SizeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+           // проверка, было бы создано поле для рисования
+           if (Canvas == null) 
+               return;
+           
+            var drawingAttributes = Canvas.DefaultDrawingAttributes; //определяет внешний вид пера (выбираются стандартные настройки)
+            drawingAttributes.Width = BrushSlider.Value; //ширина кисти
+            drawingAttributes.Height = BrushSlider.Value; //высота кисти
+        }
+    }
+
+}
